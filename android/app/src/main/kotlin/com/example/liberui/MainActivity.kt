315b7c678fa87e03ebd6e3dbe19f2fca4851477e
sagar9898 <sagarkansara9898@gmail.com>
2020-07-07 09:39:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/liberui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.liberui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:liberui/size_config.dart';
+
+void main() {
+  runApp( MaterialApp(
+      home:MyApp(),
+    theme: ThemeData(
+      textTheme: TextTheme(
+        title: TextStyle(
+          fontFamily: "Nunito"
+        ),
+        headline: TextStyle(
+          fontFamily: "Nunito",
+          fontSize: SizeConfig.textMultiplier*0.8
+
+        )
+      )
+    ),
+  ));
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Furniture App',
+              home: RestaurantScreen(),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+class RestaurantScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child:Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Text("XYZ")
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.list),title: Text('Current Order',style:Theme.of(context).textTheme.headline,),),
+          BottomNavigationBarItem(icon: Icon(Icons.home),title: Text('Go to Home',style:Theme.of(context).textTheme.headline),),
+          BottomNavigationBarItem(icon: Icon(Icons.content_paste),title: Text('Past Order',style:Theme.of(context).textTheme.headline),)
+        ]),
+      ),
+    );
+  }
+}
